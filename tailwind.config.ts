31907ba09eb8c08c387ff2fbb3857961e18f4fad
Aryan Khurana <aryankhurana1511@gmail.com>
2024-09-21 15:07:17 -0400

    Add DaisyUI + NextUI Config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
+import daisyui from "daisyui";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -34,6 +37,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui, nextui()],
 };
 export default config;
