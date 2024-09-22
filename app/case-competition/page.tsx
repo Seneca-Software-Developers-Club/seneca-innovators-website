eccdd28adeba459e9404743de824643edbdfa372
Abdulmuhsin Baksh <muhsin.baksh04@gmail.com>
2024-09-21 20:21:40 -0400

    adding FAQ to page.tsx for case competition

--- app/case-competition/page.tsx
+++ app/case-competition/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CaseCompetitionBanner from "../components/CaseCompetitionBanner/CaseCompetitionBanner";
+import FAQ from "../components/FAQ/FAQ";
 
 const CaseCompetitionPage: React.FC = () => {
   return (
     <div className="pt-16">
       <CaseCompetitionBanner />
+      <FAQ/>
     </div>
   );
 };
