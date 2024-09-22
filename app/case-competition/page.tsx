292127ec763b40f10b5d2ba50097bf125963ccae
Neel Patel <124659651+NeelinGithub@users.noreply.github.com>
2024-09-22 19:48:23 -0400

    15 what is ovh cloud section (#24)
    * Ovh cloud
    
    * fixed package.log issue; updated the component & removed animation temporarily; updated spacing and logos
    
    ---------
    
    Co-authored-by: Shrey Bhatt <shreybhatt130@yahoo.com>

--- app/case-competition/page.tsx
+++ app/case-competition/page.tsx
@@ -5,12 +5,14 @@ import CaseCompIntro from "../components/CaseCompIntro/CaseCompIntro";
 import CaseCompActionFlow from "../components/CaseCompActionFlow/CaseCompActionFlow";
 import CaseCompPrize from "../components/CaseCompPrize/CaseCompPrize";
 import FAQ from "../components/FAQ/FAQ";
+import Ovhcloudsection from "../components/Ovhcloudsection/Ovhdescription";
 
 const CaseCompetitionPage: React.FC = () => {
   return (
     <div className="mt-[48px]">
       <CaseCompBanner />
       <CaseCompPrize />
+      <Ovhcloudsection />
       <CaseCompIntro />
       <CaseCompActionFlow />
       <Sponsors />
