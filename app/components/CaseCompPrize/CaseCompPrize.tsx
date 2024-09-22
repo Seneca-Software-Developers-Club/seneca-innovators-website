54d8c391d0ff34a7f1c287ac6b8b9cb604e32a37
Shrey Bhatt <shreybhatt130@yahoo.com>
2024-09-22 16:16:56 -0400

    fixed all of the minor issues

--- app/components/CaseCompPrize/CaseCompPrize.tsx
+++ app/components/CaseCompPrize/CaseCompPrize.tsx
@@ -3,7 +3,7 @@ import { FaTrophy } from "react-icons/fa";
 
 const CaseCompPrize: React.FC = () => {
   return (
-    <div className="bg-white shadow-md rounded-xl p-8 md:p-28 max-w-full mx-auto text-center border-t-4 border-[#9E2424]">
+    <div className="bg-white shadow-md rounded-xl p-8 md:p-28 max-w-full mx-auto text-center border-t-8 border-[#9E2424]">
       <div className="flex items-center justify-center mb-6">
         <FaTrophy className="text-9xl text-yellow-500 mb-4" />
       </div>
@@ -11,8 +11,8 @@ const CaseCompPrize: React.FC = () => {
         Win <span className="text-[#b52828]">$1000</span> in Prizes
       </h2>
       <p className="text-lg text-gray-800 mb-6">
-        Participate for a chance to win up to $100 in cash prizes. Showcase your
-        skills and get recognized by industry leaders.
+        Participate for a chance to win up to $1000 in cash prizes. Showcase
+        your skills and get recognized by industry leaders.
       </p>
       <button className="bg-[#b52828] hover:bg-[#E63946] text-white font-semibold py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
         Join Now
