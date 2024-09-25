a12c463ebacf546252b2d6178801dfd8c38308f1
Harsh Patel <hpatel371@myseneca.ca>
2024-09-25 13:10:28 -0400

    HomePage Clean Code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "@/app/Home.module.css";
 
 const Home = () => {
   return (
@@ -9,51 +8,51 @@ const Home = () => {
       <br />
       <br />
 
-      <div className={styles.container}>
-        <div className={styles.backgroundOverlay}></div>
-        <main className={styles.main}>
-          <h1 className={styles.title}>SENECA INNOVATORS</h1>
-          <div className={styles.logoContainer}>
+      <div className="home-container">
+        <div className="home-background-overlay"></div>
+        <main className="home-main">
+          <h1 className="home-title">SENECA INNOVATORS</h1>
+          <div className="home-logo-container">
             <Link
               href="https://www.instagram.com/senecabusinessclub/"
               target="_blank"
               rel="noopener noreferrer"
-              className={styles.logoLink}
+              className="home-logo-link"
             >
               <Image
                 src="/logo/SBC.png"
                 alt="Seneca Business Club"
                 width={300}
                 height={300}
-                className={styles.logo}
+                className="home-logo"
               />
             </Link>
             <Link
               href="https://www.instagram.com/smarketingclub/"
               target="_blank"
               rel="noopener noreferrer"
-              className={styles.logoLink}
+              className="home-logo-link"
             >
               <Image
                 src="/logo/SMC.png"
                 alt="Seneca Marketing Club"
                 width={300}
                 height={300}
-                className={styles.logo}
+                className="home-logo"
               />
             </Link>
             <Link
               href="https://www.instagram.com/seneca_sdc/"
               target="_blank"
               rel="noopener noreferrer"
-              className={styles.logoLink}
+              className="home-logo-link"
             >
               <Image
                 src="/logo/SDC.png"
                 alt="Seneca Software Developers Club"
                 width={300}
                 height={300}
-                className={styles.logo}
+                className="home-logo"
               />
             </Link>
           </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,6 +30,21 @@ const config: Config = {
         height: "height",
         spacing: "margin, padding",
       },
+
+      keyframes: {
+        fadeInUp: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fadeInUp: "fadeInUp 1s ease-out",
+        fadeIn: "fadeIn 1.5s ease-out",
+      },
     },
     variants: {
       extend: {
